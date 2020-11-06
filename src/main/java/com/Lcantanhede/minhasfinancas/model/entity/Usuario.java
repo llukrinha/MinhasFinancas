e8b6bdf950d843bc6e175e5c5457389af6bb1b31
Lucas Cantanhêde <llukrinha@gmail.com>
2020-11-06 11:05:06 -0300

    mapeando entidade Usuario

---
+++ src/main/java/com/Lcantanhede/minhasfinancas/model/entity/Usuario.java
@@ -0,0 +1,34 @@
+package com.Lcantanhede.minhasfinancas.model.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table (name = "usuario", schema = "financas")
+@Data
+public class Usuario {
+
+    @Column (name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+
+    @Column (name = "nome")
+    @NotBlank
+    @Size(max = 255)
+    private String nome;
+
+    @Column (name = "email")
+    @NotBlank
+    @Email
+    private String email;
+
+    @Column (name = "senha")
+    @NotBlank
+    @Size(min = 6)
+    private String senha;
+}
