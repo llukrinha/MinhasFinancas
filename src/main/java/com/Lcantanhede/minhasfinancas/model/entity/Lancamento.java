147da7a8e3c9108cf1a8bb415b07170c3031c7c6
Lucas Cantanhêde <llukrinha@gmail.com>
2020-11-06 11:05:30 -0300

    mapeando entidade Lancamento

---
+++ src/main/java/com/Lcantanhede/minhasfinancas/model/entity/Lancamento.java
@@ -0,0 +1,51 @@
+package com.Lcantanhede.minhasfinancas.model.entity;
+
+import lombok.Data;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "lancamento", schema = "financas")
+@Data
+public class Lancamento {
+
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+
+    @Column(name = "descricao")
+    @NotBlank
+    @Size(max = 255)
+    private String descricao;
+
+    @Column(name = "mes")
+    private Integer mes;
+
+    @Column(name = "ano")
+    private Integer ano;
+
+    @ManyToOne
+    @JoinColumn(name = "id_usuario")
+    private Usuario usuario;
+
+    @Column(name = "valor")
+    private BigDecimal valor;
+
+    @Column(name = "data_cadastro")
+    @Convert(converter = Jsr310JpaConverters.LocalDateConverter.class)
+    private LocalDate dataCadastro;
+
+    @Column(name = "tipo")
+    @Enumerated(value = EnumType.STRING)
+    private TipoLancamento tipo;
+
+    @Column(name = "status")
+    @Enumerated(value = EnumType.STRING)
+    private StatusLancamento status;
+}
