96ffc5ebaa38f2e0e564d33b3f12160e21ec9c79
Lucas Cantanhêde <llukrinha@gmail.com>
2020-11-09 10:59:39 -0300

    criando metodo de validação de email

--- src/main/java/com/lcantanhede/minhasfinancas/service/impl/UsuarioServiceImpl.java
+++ src/main/java/com/lcantanhede/minhasfinancas/service/impl/UsuarioServiceImpl.java
@@ -1,16 +1,19 @@
 package com.lcantanhede.minhasfinancas.service.impl;
 
+import com.lcantanhede.minhasfinancas.exception.RegraNegocioException;
 import com.lcantanhede.minhasfinancas.model.entity.Usuario;
 import com.lcantanhede.minhasfinancas.model.repository.UsuarioRepository;
 import com.lcantanhede.minhasfinancas.service.UsuarioService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
 public class UsuarioServiceImpl implements UsuarioService {
 
-    private UsuarioRepository repository;
-
-    public UsuarioServiceImpl(UsuarioRepository repository) {
-        this.repository = repository;
-    }
+    private final UsuarioRepository repository;
 
     @Override
     public Usuario autenticar(String email, String senha) {
@@ -24,6 +27,9 @@ public class UsuarioServiceImpl implements UsuarioService {
 
     @Override
     public void validarEmail(String email) {
-
+        Optional<Usuario> usuario = repository.findByEmailIgnoreCase(email);
+        if (usuario.isPresent()){
+            throw new RegraNegocioException("Já existe um usuário cadastrado com este email.");
+        }
     }
 }
