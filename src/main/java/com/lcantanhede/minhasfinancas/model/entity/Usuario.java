06389a3489672958e26109a649c7cc632c503b5f
Lucas Cantanhêde <llukrinha@gmail.com>
2020-11-25 11:16:10 -0300

    refatorando entidade usuario

--- src/main/java/com/lcantanhede/minhasfinancas/model/entity/Usuario.java
+++ src/main/java/com/lcantanhede/minhasfinancas/model/entity/Usuario.java
@@ -1,15 +1,18 @@
 package com.lcantanhede.minhasfinancas.model.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
 
 @Entity
 @Table (name = "usuario", schema = "financas")
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Usuario {
 
     @Column (name = "id")
@@ -18,17 +21,11 @@ public class Usuario {
     private Long id;
 
     @Column (name = "nome")
-    @NotBlank
-    @Size(max = 255)
     private String nome;
 
     @Column (name = "email")
-    @NotBlank
-    @Email
     private String email;
 
     @Column (name = "senha")
-    @NotBlank
-    @Size(min = 6)
     private String senha;
 }
