1a75d3ff2c94c8ec0ce1d27489712b09db3df719
Lucas Cantanhêde <llukrinha@gmail.com>
2020-11-25 11:19:31 -0300

    criando classe classe dto

---
+++ src/main/java/com/lcantanhede/minhasfinancas/model/dto/UsuarioDto.java
@@ -0,0 +1,35 @@
+package com.lcantanhede.minhasfinancas.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "usuario", schema = "financas")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UsuarioDto {
+
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+
+    @Column (name = "nome")
+    @NotBlank
+    @Size(max = 255)
+    private String nome;
+
+    @Column (name = "email")
+    @NotBlank
+    @Email
+    private String email;
+}
