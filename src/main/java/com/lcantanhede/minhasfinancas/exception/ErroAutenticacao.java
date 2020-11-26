ccc524ca807a135ec05d824ae78c0bb6502b5b82
Lucas Cantanhêde <llukrinha@gmail.com>
2020-11-26 10:43:22 -0300

    implementando exception erro de autenticação

---
+++ src/main/java/com/lcantanhede/minhasfinancas/exception/ErroAutenticacao.java
@@ -0,0 +1,7 @@
+package com.lcantanhede.minhasfinancas.exception;
+
+public class ErroAutenticacao extends RuntimeException{
+    public ErroAutenticacao(String mensagem){
+        super(mensagem);
+    }
+}
