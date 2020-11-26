1b84bac02e111f4d977f8824f852ea37ae009361
Lucas Cantanhêde <llukrinha@gmail.com>
2020-11-26 11:35:12 -0300

    implementando test com banco de dados

--- src/test/java/com/lcantanhede/minhasfinancas/model/repository/UsuarioRepositoryTest.java
+++ src/test/java/com/lcantanhede/minhasfinancas/model/repository/UsuarioRepositoryTest.java
@@ -6,24 +6,40 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.Optional;
+
 @ActiveProfiles("test")
 @ExtendWith(SpringExtension.class)
-@SpringBootTest
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class UsuarioRepositoryTest {
 
     @Autowired
     UsuarioRepository usuarioRepository;
 
+    @Autowired
+    TestEntityManager entityManager;
+
+    static Usuario criarUsuario() {
+        return Usuario.builder()
+                .nome("usuario")
+                .senha("senha")
+                .email("usuario@email.com").build();
+    }
+
     @Test
     @DisplayName("Deve verificar a existencia de um email")
     void verificarEmail() {
 
-        Usuario usuario = Usuario.builder().nome("usuario").email("usuario@email.com").build();
-        usuarioRepository.save(usuario);
+        Usuario usuario = criarUsuario();
+
+        entityManager.persist(usuario);
 
         boolean result = usuarioRepository.existsByEmail("usuario@email.com");
 
@@ -34,10 +50,44 @@ class UsuarioRepositoryTest {
     @DisplayName("Deve retornar falso quando não houver usuario cadastrado com o email")
     void retornoFalsoUsuarioEmail() {
 
-        usuarioRepository.deleteAll();
-
         boolean result = usuarioRepository.existsByEmail("usuario@email.com");
 
         Assertions.assertThat(result).isFalse();
     }
+
+    @Test
+    @DisplayName("Deve persistir um usuario da base de dados")
+    void persistirUsuarioBd() {
+
+        Usuario usuario = criarUsuario();
+
+        Usuario usuarioSalvo = usuarioRepository.save(usuario);
+
+        Assertions.assertThat(usuarioSalvo.getId()).isNotNull();
+    }
+
+    @Test
+    @DisplayName("Deve buscar um usuario por email")
+    void buscarUsuarioEmail() {
+
+        Usuario usuario = criarUsuario();
+        entityManager.persist(usuario);
+
+        Optional<Usuario> result = usuarioRepository.findByEmail("usuario@email.com");
+
+        Assertions.assertThat(result.isPresent()).isTrue();
+    }
+
+    @Test
+    @DisplayName("Deve retornar vazio ao buscar usuario por email quando nao existe na base")
+    void buscarUsuarioEmailVazio() {
+
+        Usuario usuario = criarUsuario();
+        entityManager.persist(usuario);
+
+        Optional<Usuario> result = usuarioRepository.findByEmail("usuario@email.com");
+
+        Assertions.assertThat(result.isPresent()).isTrue();
+    }
+
 }
