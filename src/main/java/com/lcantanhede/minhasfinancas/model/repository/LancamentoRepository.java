1249f2767fa49ae392edace791c3b944e423814b
Lucas Cantanhêde <llukrinha@gmail.com>
2020-12-07 11:43:08 -0300

    implementando query metodo na classe interface do lancamento

--- src/main/java/com/lcantanhede/minhasfinancas/model/repository/LancamentoRepository.java
+++ src/main/java/com/lcantanhede/minhasfinancas/model/repository/LancamentoRepository.java
@@ -3,5 +3,9 @@ package com.lcantanhede.minhasfinancas.model.repository;
 import com.lcantanhede.minhasfinancas.model.entity.Lancamento;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface LancamentoRepository extends JpaRepository<Lancamento, Long> {
+
+    List<Lancamento>findByDescricaoIgnoreCaseContaining(String descricao);
 }
