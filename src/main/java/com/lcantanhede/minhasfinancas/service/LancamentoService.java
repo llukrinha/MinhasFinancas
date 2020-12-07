3e89906acfdee96179660556e2cb58e86599da77
Lucas Cantanhêde <llukrinha@gmail.com>
2020-12-07 11:42:33 -0300

    implementando validações metodo de serviço do lancamento

---
+++ src/main/java/com/lcantanhede/minhasfinancas/service/LancamentoService.java
@@ -0,0 +1,81 @@
+package com.lcantanhede.minhasfinancas.service;
+
+import com.lcantanhede.minhasfinancas.exception.RegraNegocioException;
+import com.lcantanhede.minhasfinancas.model.entity.Lancamento;
+import com.lcantanhede.minhasfinancas.model.enums.StatusLancamento;
+import com.lcantanhede.minhasfinancas.model.repository.LancamentoRepository;
+import org.hibernate.criterion.Example;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Objects;
+
+public class LancamentoService {
+
+    private final LancamentoRepository lancamentoRepository;
+
+    public LancamentoService(LancamentoRepository lancamentoRepository) {
+        this.lancamentoRepository = lancamentoRepository;
+    }
+
+    @Autowired
+    @Transactional
+    public Lancamento salvar(Lancamento lancamento) {
+        validar(lancamento);
+        lancamento.setStatus(StatusLancamento.PENDENTE);
+        return lancamentoRepository.save(lancamento);
+    }
+
+    @Autowired
+    public Lancamento atualizar(Lancamento lancamento) {
+        Objects.requireNonNull(lancamento.getId());
+        validar(lancamento);
+        return lancamentoRepository.save(lancamento);
+    }
+
+    @Autowired
+    @Transactional
+    void deletar(Lancamento lancamento) {
+        Objects.requireNonNull(lancamento.getId());
+        lancamentoRepository.delete(lancamento);
+    }
+
+    @Autowired
+    @Transactional
+    public List<Lancamento> buscar(Lancamento descricao) {
+
+        return lancamentoRepository.findByDescricaoIgnoreCaseContaining(descricao.getDescricao());
+    }
+
+    @Autowired
+    @Transactional
+    void atualizarStatus(Lancamento lancamento, StatusLancamento statusLancamento) {
+        lancamento.setStatus(statusLancamento);
+        atualizar(lancamento);
+    }
+
+    @Autowired
+    public void validar(Lancamento lancamento) {
+        if (lancamento.getDescricao() == null || lancamento.getDescricao().trim().equals("")) {
+            throw new RegraNegocioException("Informe uma Descrição válida.");
+        }
+        if (lancamento.getMes() == null || lancamento.getMes() < 1 || lancamento.getMes() > 12) {
+            throw new RegraNegocioException("Informe um Mês válido.");
+        }
+        if (lancamento.getAno() == null || lancamento.getAno().toString().length() != 4) {
+            throw new RegraNegocioException("Informe um Ano válido.");
+        }
+        if (lancamento.getUsuario() == null || lancamento.getUsuario().getId() == null) {
+            throw new RegraNegocioException("Informe um Usuário.");
+        }
+        if (lancamento.getValor() == null || lancamento.getValor().compareTo(BigDecimal.ZERO) < 1) {
+            throw new RegraNegocioException("Informe um Valor válido.");
+        }
+        if (lancamento.getTipo() == null) {
+            throw new RegraNegocioException("Informe um tipo de Lançamento");
+        }
+    }
+}
